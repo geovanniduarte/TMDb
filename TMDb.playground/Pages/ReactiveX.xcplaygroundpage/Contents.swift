c62af9afee70625097d33879d5082cdf5e391559
Geo <geovanni.duarte.guerrero@gmail.com>
2018-06-06 18:29:19 -0500

    playground de rx

--- TMDb.playground/Pages/ReactiveX.xcplaygroundpage/Contents.swift
+++ TMDb.playground/Pages/ReactiveX.xcplaygroundpage/Contents.swift
@@ -2,6 +2,9 @@
 
 import TMDbCore
 import RxSwift
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 enum APIError : Error {
     case invalidKey
@@ -14,5 +17,31 @@ let error = Observable<Int>.error(APIError.invalidKey)
 let single = Observable.just("Hola k ase, programa o k ase")
 
 let hello = Observable<String>.create { observer in
+    observer.onNext("Hello")
+    observer.onNext("World")
+    oberver.onCompleted()
+    return Disposables.create()
+}
+
+let promise = Observable.create { observer in
+    observer.onNext("Hello")
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+        observer.onNext("Five second later")
+        observer.onCompleted()
+    }
+    
+    return Disposables.create()
+}
+
+promise.subscribe { event in
+    switch event {
+    case .next(let value):
+        print("next(\(value)")
+    case .error(let error):
+        print("next(\(error)")
+    default:
+        print("completed")
+    }
     
 }
